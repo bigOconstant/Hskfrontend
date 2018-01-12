03f27416ef9bfba4ae7df1d2ced1ed587e8fdbd3
Caleb McCarthy <caleb.a.mccarthy@gmail.com>
2018-01-11 21:40:01 -0600

    Adding landing page to lessons. Some auto text formatting

--- src/app/lessons/lessons.module.ts
+++ src/app/lessons/lessons.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Lesson1Component } from './lesson1/lesson1.component';
 import {lessonsrouting} from './lessons.routing';
+import { LessonLandingComponent } from './lesson-landing/lesson-landing.component';
 
 @NgModule({
   imports: [
     CommonModule,
     lessonsrouting
   ],
-  declarations: [Lesson1Component]
+  declarations: [Lesson1Component, LessonLandingComponent]
 })
 export class LessonsModule { }
