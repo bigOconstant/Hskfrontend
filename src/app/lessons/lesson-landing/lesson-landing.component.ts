f2a7b80a5ef3793f49875fa555723734e4eb541c
Caleb McCarthy <caleb.a.mccarthy@gmail.com>
2018-01-11 21:40:01 -0600

    Adding landing page to lessons. Some auto text formatting

---
+++ src/app/lessons/lesson-landing/lesson-landing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lesson-landing',
+  templateUrl: './lesson-landing.component.html',
+  styleUrls: ['./lesson-landing.component.css']
+})
+export class LessonLandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
